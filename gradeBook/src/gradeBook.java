231a81a0588f0f554dc4f02f73e4c8295c35219e
leomont <leonardo.montesm@gmail.com>
2024-10-09 19:34:30 -0500

    Agregar logica de clase gradeBook

--- gradeBook/src/gradeBook.java
+++ gradeBook/src/gradeBook.java
@@ -1,11 +1,21 @@
 public class gradeBook {
 
+    private String courseName = "";
+
     public gradeBook() {
     }
 
-    public void displayMessage()
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void displayMessage(String nombreCurso)
     {
-        System.out.println("Programacion orientada a objetos");
+        System.out.println("curso ingresado: " + nombreCurso);
     }
 
 
