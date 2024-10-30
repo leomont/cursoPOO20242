36c4a1862f60605a17fbff888339c0a90c5f6319
David Bulla <73729459+daprionil@users.noreply.github.com>
2024-10-30 04:43:46 +0000

    fix: modifica mensaje de débito

--- cursoPOO20242/src/Account/AccountTest.java
+++ cursoPOO20242/src/Account/AccountTest.java
@@ -40,7 +40,7 @@ public class AccountTest {
       double debitAmount = input.nextDouble();
       
       if (debitAmount > account1.getBalance()){
-         System.out.printf("%nextract %.2f amount is invalid to account1 balance%n%n", 
+         System.out.printf("%El monto del débito [ %.2f] excedió el saldo de la cuenta %n%n", 
          debitAmount);
          account1.debit(debitAmount); // extract amount to account1 balance
       }else{
@@ -56,7 +56,7 @@ public class AccountTest {
       debitAmount = input.nextDouble();
       
       if (debitAmount > account2.getBalance()){
-         System.out.printf("%nextract %.2f amount is invalid to account2 balance%n%n", 
+         System.out.printf("%El monto del débito [ %.2f] excedió el saldo de la cuenta %n%n", 
          debitAmount);
          account2.debit(debitAmount); // extract amount to account2 balance
       }else{
