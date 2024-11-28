8c3e7f4002136c2bc351a610620064749ba6080e
leomont <leonardo.montesm@gmail.com>
2024-11-27 21:32:09 -0500

    Agregar demo de formularios

---
+++ cursoPOO20242/src/Formulario3.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Formulario3 {
+    private JTextField textField1;
+    private JButton button1;
+}
---
+++ cursoPOO20242/src/Formulario4.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Formulario4 {
+    private JButton button1;
+    private JRadioButton radioButton1;
+    private JTextField dsdsadsadTextField;
+}
